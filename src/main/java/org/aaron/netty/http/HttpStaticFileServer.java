840fe7aaa64096d9c2a561d82bb13df9c2c14e4f
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-03-23 09:29:45 -0500

    Add code.

---
+++ src/main/java/org/aaron/netty/http/HttpStaticFileServer.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.aaron.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class HttpStaticFileServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HttpStaticFileServer.class);
+
+    private static final boolean SSL = System.getProperty("ssl") != null;
+    private static final int PORT = Integer.parseInt(System.getProperty("port", SSL ? "8443" : "8080"));
+
+    public static void main(String[] args) throws Exception {
+        LOG.info("begin main");
+
+        // Configure SSL.
+        final SslContext sslCtx;
+        if (SSL) {
+            final var ssc = new SelfSignedCertificate();
+            sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey())
+                    .sslProvider(SslProvider.JDK).build();
+        } else {
+            sslCtx = null;
+        }
+
+        final var bossGroup = new NioEventLoopGroup(1);
+        final var workerGroup = new NioEventLoopGroup();
+        try {
+            final var b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpStaticFileServerInitializer(sslCtx));
+
+            final var ch = b.bind(PORT).sync().channel();
+
+            LOG.info("Open your web browser and navigate to " +
+                    (SSL ? "https" : "http") + "://127.0.0.1:" + PORT + '/');
+
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
