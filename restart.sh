c61bbd80724daf22bbd9c60f19f3cc65fd312507
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-03-30 11:22:03 -0500

    Add OpenBSD jdk path.

--- restart.sh
+++ restart.sh
@@ -4,4 +4,8 @@ CONFIG_FILE=./$(hostname -s)-config.json
 
 pkill -U $(id -u) java
 
+if [ $(uname) = 'OpenBSD' ]; then
+  export PATH="/usr/local/jdk-1.8.0/bin/:$PATH"
+fi
+
 nohup java -Dconfig.file.name=$CONFIG_FILE -jar build/libs/*.jar 2>&1 | svlogd logs &
