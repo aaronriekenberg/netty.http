54ff36586a1ce760957508442b068fbdeb645fbf
Aaron Riekenberg <aaron.riekenberg@target.com>
2019-04-09 20:21:32 -0700

    Install ThreadFactory for blockingThreadPool.

--- src/main/kotlin/org/aaron/netty/http/netty/ThreadPool.kt
+++ src/main/kotlin/org/aaron/netty/http/netty/ThreadPool.kt
@@ -2,9 +2,26 @@ package org.aaron.netty.http.netty
 
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 
 object BlockingThreadPoolContainer {
 
-    val blockingThreadPool: ExecutorService = Executors.newCachedThreadPool()
+    private val threadNumber = AtomicInteger(0)
+
+    val blockingThreadPool: ExecutorService = Executors.newCachedThreadPool { runnable ->
+        val thread = Thread(runnable)
+
+        thread.name = "BlockingThreadPool-${threadNumber.getAndIncrement()}"
+
+        if (thread.isDaemon) {
+            thread.isDaemon = false
+        }
+
+        if (thread.priority != Thread.NORM_PRIORITY) {
+            thread.priority = Thread.NORM_PRIORITY
+        }
+
+        thread
+    }
 
 }
