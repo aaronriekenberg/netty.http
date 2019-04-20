204fedf335d986c5ed6041812ef7c064a44d1197
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-04-20 05:52:13 -0500

    Add netty version to environment.

--- src/main/kotlin/org/aaron/netty/http/environment/Environment.kt
+++ src/main/kotlin/org/aaron/netty/http/environment/Environment.kt
@@ -3,6 +3,7 @@ package org.aaron.netty.http.environment
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
+import io.netty.util.Version
 import mu.KotlinLogging
 import java.time.Instant
 
@@ -17,6 +18,9 @@ data class Environment(
         @field:JsonProperty("env_vars")
         val envVars: Map<String, String>,
 
+        @field:JsonProperty("netty_verison")
+        val nettyVersion: Map<String, String>,
+
         @field:JsonProperty("properties")
         val properties: Map<String, String>
 )
@@ -26,6 +30,7 @@ object EnvironmentContainer {
     val environment = Environment(
             startTime = Instant.now(),
             envVars = System.getenv().toSortedMap(),
+            nettyVersion = Version.identify().mapValues { it.value.toString() },
             properties = System.getProperties().stringPropertyNames().sorted().map { it to System.getProperty(it) }.toMap()
     )
 
