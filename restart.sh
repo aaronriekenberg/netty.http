1b7f4ae48f3d7d055bad98fb2e00ca345446d502
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-04-28 07:16:49 -0500

    Set Xmx for raspberrypi.  Sleep after pkill.

--- restart.sh
+++ restart.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -x
 
 CONFIG_FILE=./$(hostname -s)-config.json
+JAVA_OPTS=
 
 pkill -U $(id -u) java
 
+sleep 2
+
 if [ $(hostname) = 'raspberrypi' ]; then
   export PATH="/home/pi/jdk-11.0.3+7/bin/:$PATH"
+  JAVA_OPTS="-Xmx64m"
 fi
 
-nohup java -Dconfig.file.name=$CONFIG_FILE -jar build/libs/*.jar 2>&1 | svlogd logs &
+nohup java -Dconfig.file.name=$CONFIG_FILE $JAVA_OPTS -jar build/libs/*.jar 2>&1 | svlogd logs &
