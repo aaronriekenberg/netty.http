cf2eec1ff5113bd742b4e84e4e23bc0a45b7d75b
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-05-19 09:54:18 -0500

    Update apu2 config for linux.

--- restart.sh
+++ restart.sh
@@ -1,15 +1,12 @@
 #!/bin/sh -x
 
 CONFIG_FILE=./$(hostname -s)-config.json
-JAVA_OPTS=
+JAVA_OPTS='-Xmx64m'
 
 pkill -U $(id -u) java
 
 sleep 2
 
-if [ $(hostname) = 'raspberrypi' ]; then
-  export PATH="/home/pi/jdk-11.0.3+7/bin/:$PATH"
-  JAVA_OPTS="-Xmx64m"
-fi
+export PATH="$HOME/jdk-11.0.3+7/bin/:$PATH"
 
 nohup java -Dconfig.file.name=$CONFIG_FILE $JAVA_OPTS -jar build/libs/*.jar 2>&1 | svlogd logs &
