84a542f3eeb836be99c6d056b5621be412e86167
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-05-19 10:03:54 -0500

    Only set Xmx on raspberrypi, clean up apu2 config.

--- restart.sh
+++ restart.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -x
 
 CONFIG_FILE=./$(hostname -s)-config.json
-JAVA_OPTS='-Xmx64m'
+JAVA_OPTS=
 
 pkill -U $(id -u) java
 
 sleep 2
 
 export PATH="$HOME/jdk-11.0.3+7/bin/:$PATH"
+if [ $(hostname) = 'raspberrypi' ]; then
+  JAVA_OPTS="-Xmx64m"
+fi
 
 nohup java -Dconfig.file.name=$CONFIG_FILE $JAVA_OPTS -jar build/libs/*.jar 2>&1 | svlogd logs &
