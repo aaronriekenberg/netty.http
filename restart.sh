c969289aac20cb8afb618111add5eddb39fc98d8
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-08-03 05:47:07 -0500

    Update path.

--- restart.sh
+++ restart.sh
@@ -7,9 +7,11 @@ pkill -U $(id -u) java
 
 sleep 2
 
-export PATH="$HOME/jdk-11.0.3+7/bin/:$PATH"
+export PATH="${HOME}/jdk-11.0.4+11/bin/:$PATH"
 if [ $(hostname) = 'raspberrypi' ]; then
   JAVA_OPTS="-Xmx64m"
 fi
+
+export PATH="${HOME}/bin:$PATH"
 
 nohup java -Dconfig.file.name=$CONFIG_FILE $JAVA_OPTS -jar build/libs/*.jar 2>&1 | svlogd logs &
